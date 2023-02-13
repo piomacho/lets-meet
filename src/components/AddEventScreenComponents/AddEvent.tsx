c5c674f12d00bc3876ccfdecf55d6932ae6ba8d9
Piotr Machowski <piotr.machowski@playbookengineering.com>
2023-02-13 22:06:31 +0100

    Form styling

--- src/components/AddEventScreenComponents/AddEvent.tsx
+++ src/components/AddEventScreenComponents/AddEvent.tsx
@@ -2,16 +2,9 @@ import * as React from 'react';
 import { useAppState } from '@src/state/appState';
 
 import { observer } from 'mobx-react-lite';
-import {
-    View,
-    ActivityIndicator,
-    StyleSheet,
-    Platform,
-    Keyboard,
-    TouchableWithoutFeedback,
-    Button,
-    Text,
-} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import { Text, Button, Input } from '@rneui/themed';
+import { View, ActivityIndicator, StyleSheet, Platform, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { Formik } from 'formik';
 import RNPickerSelect from 'react-native-picker-select';
 
@@ -67,12 +60,89 @@ export const AddEvent = observer(() => {
                     };
                     return (
                         <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss} accessible={false}>
-                            <Text>Form here</Text>
+                            <View>
+                                <Input label="Nazwa wydarzenia" onChangeText={setEventName} />
+                                {/* <Input
+                                    placeholder="Details"
+                                    label="Details"
+                                    onChangeText={setDetails}
+                                    multiline={true}
+                                    numberOfLines={4}
+                                    style={{ padding: 10 }}
+                                /> */}
+                                <Input
+                                    editable
+                                    multiline
+                                    label="Opis"
+                                    numberOfLines={5}
+                                    onChangeText={text => setDetails(text)}
+                                    value={details}
+                                    style={{ padding: 10 }}
+                                />
+                                <Input label="Liczba uczestników" keyboardType="numeric" onChangeText={setEventName} />
+                                <View style={{ margin: 10 }}>
+                                    <Text>Wybierz kategorię</Text>
+                                    <RNPickerSelect
+                                        useNativeAndroidPickerStyle={false}
+                                        placeholder={{
+                                            label: 'Wybierz kategorię',
+                                            value: null,
+                                        }}
+                                        onValueChange={(value: string) => setCategory(value)}
+                                        items={[
+                                            { label: 'Badminton', value: 'badminton' },
+                                            { label: 'Tenis', value: 'tenis' },
+                                            {
+                                                label: 'Fifa',
+                                                value: 'fifa',
+                                            },
+                                            {
+                                                label: 'Balet',
+                                                value: 'balet',
+                                            },
+                                            {
+                                                label: 'Koszykówka',
+                                                value: 'basketball',
+                                            },
+                                        ]}
+                                        style={{ ...pickerSelectStyles }}
+                                        Icon={() => <Icon name="down" size={20} color={'#000'} />}
+                                    />
+
+                                    <Text style={{ marginTop: 20 }}>Wybierz płeć uczestników</Text>
+                                    <RNPickerSelect
+                                        useNativeAndroidPickerStyle={false}
+                                        onValueChange={(value: string) => setGender(value)}
+                                        placeholder={{
+                                            label: 'Wybierz płeć',
+                                            value: null,
+                                        }}
+                                        items={[
+                                            { label: 'Wszyscy są mile widziani !', value: 'all' },
+                                            { label: 'Kobiety', value: 'female' },
+                                            {
+                                                label: 'Mężczyzni',
+                                                value: 'male',
+                                            },
+                                            {
+                                                label: 'Zdefiniowana w opisie',
+                                                value: 'unknown',
+                                            },
+                                        ]}
+                                        style={{ ...pickerSelectStyles }}
+                                        Icon={() => <Icon name="down" size={20} color={'#000'} />}
+                                    />
+                                </View>
+                            </View>
                         </TouchableWithoutFeedback>
                     );
                 }}
             </Formik>
-            <Button title="Next" onPress={routerState.navigateToInviteGuests} />
+            <Button
+                title="Next"
+                style={{ width: 100, alignSelf: 'center', marginTop: 20 }}
+                onPress={routerState.navigateToInviteGuests}
+            />
         </View>
     );
 });
