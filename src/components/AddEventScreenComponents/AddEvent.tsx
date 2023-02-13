bd135fd0c7720545e8d19ad76105c00a58ffa259
Piotr Machowski <piotr.machowski@playbookengineering.com>
2023-02-13 20:52:38 +0100

    Create event state

--- src/components/AddEventScreenComponents/AddEvent.tsx
+++ src/components/AddEventScreenComponents/AddEvent.tsx
@@ -1,15 +1,92 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
 import { useAppState } from '@src/state/appState';
+
 import { observer } from 'mobx-react-lite';
+import {
+    View,
+    ActivityIndicator,
+    StyleSheet,
+    Platform,
+    Keyboard,
+    TouchableWithoutFeedback,
+    Button,
+    Text,
+} from 'react-native';
+import { Formik } from 'formik';
+import RNPickerSelect from 'react-native-picker-select';
+
+import { AddEventState } from '@src/state/addEventState';
 
 export const AddEvent = observer(() => {
     const { routerState } = useAppState();
 
+    const [addEventState] = React.useState(() => new AddEventState());
+    const {
+        error,
+        successMessage,
+        date,
+        isLoading,
+        eventName,
+        details,
+        numberOfPeople,
+        gender,
+        category,
+        eventNameStatus,
+        detailsStatus,
+        numberOfPeopleStatus,
+        categoryStatus,
+        isFormValid,
+        setEventName,
+        setCategory,
+        setNumberOfPeople,
+        setDetails,
+        setGender,
+        onDateChange,
+        createEvent,
+        validate,
+    } = addEventState;
+
+    const isIos = Platform.OS === 'ios';
     return (
-        <View style={{ alignItems: 'center' }}>
-            <Text>AddEventScreen</Text>
+        <View>
+            <Formik
+                initialValues={{
+                    eventName: '',
+                    details: '',
+                    numberOfPeople: 2,
+                    category: '',
+                }}
+                onSubmit={() => {
+                    createEvent();
+                }}>
+                {({ handleSubmit }) => {
+                    const submit = () => {
+                        if (isFormValid) {
+                            handleSubmit();
+                        }
+                    };
+                    return (
+                        <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss} accessible={false}>
+                            <Text>Form here</Text>
+                        </TouchableWithoutFeedback>
+                    );
+                }}
+            </Formik>
             <Button title="Next" onPress={routerState.navigateToInviteGuests} />
         </View>
     );
+});
+
+export const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 16,
+        color: '#91919F',
+    },
+    inputAndroid: {
+        fontSize: 16,
+        color: '#91919F',
+    },
+    placeholder: {
+        color: '#91919F',
+    },
 });
---
+++ src/state/signUpState.ts
@@ -0,0 +1,170 @@
+import moment from 'moment';
+import { makeAutoObservable } from 'mobx';
+
+export class SignUptState {
+    date: Date | null = null;
+    error: string | null = null;
+    successMessage: string | null = null;
+    isLoading: boolean = false;
+
+    public firstName: string = '';
+    public lastName: string = '';
+    public postCode: string = '';
+    public email: string = '';
+    public password: string = '';
+    public confirmPassword: string = '';
+    public gender: string = '';
+
+    public firstNameStatus: boolean | null = null;
+    public lastNameStatus: boolean | null = null;
+    public genderStatus: boolean | null = null;
+    public postCodeStatus: boolean | null = null;
+    public emailStatus: boolean | null = null;
+    public passwordStatus: boolean | null = null;
+    public confirmPasswordStatus: boolean | null = null;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    public setDate = (currentDate: Date | null) => {
+        this.date = currentDate;
+    };
+    public setError = (error: string | null) => {
+        this.error = error;
+    };
+
+    public setSuccessMessage = (successMessage: string) => {
+        this.successMessage = successMessage;
+    };
+
+    public setLoading = (status: boolean) => {
+        this.isLoading = status;
+    };
+
+    public setFirstName = (name: string) => {
+        this.firstName = name;
+    };
+    public setGender = (name: string) => {
+        this.gender = name;
+    };
+
+    public setLastName = (lastName: string) => {
+        this.lastName = lastName;
+    };
+
+    public setPostCode = (postcode: string) => {
+        this.postCode = postcode;
+    };
+
+    public setEmail = (email: string) => {
+        this.email = email.toLowerCase();
+    };
+    public setPassword = (password: string) => {
+        this.password = password;
+    };
+    public setConfirmPassword = (password: string) => {
+        this.confirmPassword = password;
+    };
+
+    validateEmail = (email: string) => {
+        return /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+    };
+
+    validate = (value: string) => {
+        if (value === 'name') {
+            this.firstNameStatus = this.firstName.length > 0;
+        }
+        if (value === 'surname') {
+            this.lastNameStatus = this.lastName.length > 0;
+        }
+        if (value === 'email') {
+            this.emailStatus = this.validateEmail(this.email);
+        }
+        if (value === 'postcode') {
+            this.postCodeStatus = this.postCode.length > 4 && this.postCode.length < 9;
+        }
+
+        if (value === 'password') {
+            this.passwordStatus = this.password.length > 7;
+        }
+        if (value === 'passwordConfirm') {
+            this.confirmPasswordStatus = this.confirmPassword.length > 7;
+        }
+    };
+
+    public onDateChange = (selectedDate: Date | undefined) => {
+        const currentDate = selectedDate;
+        if (currentDate !== undefined) {
+            this.setDate(currentDate);
+        }
+    };
+
+    public get dateForSignup(): string {
+        return moment(this.date).format('DD-MM-YYYY');
+    }
+
+    public get isGenderValid(): boolean {
+        return this.gender !== '';
+    }
+    public get isDateValid(): boolean {
+        return this.date !== null;
+    }
+
+    public get isFormValid(): boolean {
+        return (
+            this.firstNameStatus === true &&
+            this.lastNameStatus === true &&
+            this.emailStatus === true &&
+            this.postCodeStatus === true &&
+            this.passwordStatus === true &&
+            this.confirmPasswordStatus === true &&
+            this.isGenderValid === true &&
+            this.isDateValid === true
+        );
+    }
+
+    onEmailRegisterButtonPress = async (email: string, password: string) => {
+        try {
+            console.log(email, password);
+        } catch (error: unknown) {
+            //@ts-expect-error
+            this.setError(firebaseErrors[error.code]);
+        }
+    };
+
+    registerUser = async () => {
+        try {
+            const fields = ['name', 'surname', 'email', 'postcode', 'password', 'password-confirm'];
+
+            fields.forEach(field => {
+                this.validate(field);
+            });
+            if (this.isFormValid === false) {
+                return;
+            }
+
+            const password = this.password;
+            if (password !== this.confirmPassword) {
+                this.setError('Passwords do not match');
+                return;
+            }
+
+            const minAgeDate = moment().subtract(13, 'years');
+            const usersDate = moment(this.date);
+
+            if (usersDate.isBefore(minAgeDate) === false) {
+                this.setError('You have to be 13+ years old to create a new account');
+                return;
+            }
+
+            this.setLoading(true);
+
+            await this.onEmailRegisterButtonPress(this.email, this.password);
+
+            this.setLoading(false);
+        } catch (err) {
+            this.setLoading(false);
+        }
+    };
+}
